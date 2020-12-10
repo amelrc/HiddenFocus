b3e21b7a6aba94b3cc5fb36a341feff2194ab871
Amel Ramirez <amelramirez@192.168.2.4>
2020-12-10 21:20:26 +0100

    scroll horizontal

--- src/components/WhatNow.js
+++ src/components/WhatNow.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React from 'react';
+import HorizontalScroll from 'react-scroll-horizontal';
 
 const WhatNow = () => {
-  return (
-    <div>
-      <h1>What Now Page</h1>
-    </div>
-  );
+	const child = { width: `30em`, height: `100%`, backgroundColor: 'red' };
+
+	return (
+		<div style={{ backgroundColor: 'aqua', padding: 40 }}>
+			<HorizontalScroll>
+				<div style={child}>
+					<h1>hello</h1>
+				</div>
+				<div style={child}>
+					<h1>hello</h1>
+				</div>
+				<div style={child}>
+					<h1>hello</h1>
+				</div>
+			</HorizontalScroll>
+		</div>
+	);
 };
 
 export default WhatNow;
