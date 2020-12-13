6e8c05c45c9f512001c95ae46bbe59e0c15d4c9b
Amel Ramirez <amelramirez@192.168.2.4>
2020-12-13 09:21:36 +0100

    scrolling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,66 +1,66 @@
-import React from "react";
-import styled from "styled-components";
-import { motion } from "framer-motion";
-import { Link } from "react-router-dom";
-import "../App.css";
+import React from 'react';
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+import { Link } from 'react-router-dom';
+import '../App.css';
 
-import Menu from "./Menu";
-import ImgHome from "../images/WEB 0789-1500x1000--517-kb.jpg";
+import Menu from './Menu';
+import ImgHome from '../images/WEB 0789-1500x1000--517-kb.jpg';
 
 const HeaderHome = styled.h1`
-  text-align: left;
-  // font: normal normal 100 78px/40px Roxborough CF;
-  color: #331c65;
-  text-transform: capitalize;
+	text-align: left;
+	// font: normal normal 100 78px/40px Roxborough CF;
+	color: #331c65;
+	text-transform: capitalize;
 `;
 const SubTitle = styled.h4`
-  text-align: left;
-  font: normal normal normal 22px/27px Lato;
-  color: #331c65;
-  text-transform: capitalize;
+	text-align: left;
+	font: normal normal normal 22px/27px Lato;
+	color: #331c65;
+	text-transform: capitalize;
 `;
 const Phrase = styled.h3`
-  text-align: left;
-  font: normal normal normal 46px/56px Mrs Saint Delafield;
-  letter-spacing: 0px;
-  color: #1c161f;
+	text-align: left;
+	font: normal normal normal 46px/56px Mrs Saint Delafield;
+	letter-spacing: 0px;
+	color: #1c161f;
 `;
 
 export const PageTransition = {
-  in: {
-    opacity: 1,
-    // x: 0,
-  },
-  out: {
-    opacity: 0,
-    // x: "-100vh",
-  },
+	in: {
+		opacity: 1,
+		// x: 0,
+	},
+	out: {
+		opacity: 0,
+		// x: "-100vh",
+	},
 };
 const Home = () => {
-  return (
-    <>
-      <motion.div
-        initial="out"
-        animate="in"
-        exit="out"
-        variants={PageTransition}
-        style={{ alignSelf: "center" }}
-      >
-        <Menu />
-        <div>
-          <HeaderHome>hidden focus</HeaderHome>
-          <SubTitle>photography by ray hanson</SubTitle>
-        </div>
+	return (
+		<>
+			<motion.div
+				initial='out'
+				animate='in'
+				exit='out'
+				variants={PageTransition}
+				style={{ alignSelf: 'center' }}
+			>
+				<Menu />
+				<div>
+					<HeaderHome>hidden focus</HeaderHome>
+					<SubTitle>photography by ray hanson</SubTitle>
+				</div>
 
-        <Link to="/gallery">
-          <div className="img-hover-zoom">
-            <img style={{ width: "100%" }} src={ImgHome} alt="home" />
-          </div>
-        </Link>
+				<Link to='/gallery'>
+					<div className='img-hover-zoom'>
+						<motion.img style={{ width: '100%' }} src={ImgHome} alt='home' />
+					</div>
+				</Link>
 
-        <Phrase>Each of our lives is a different way of seeing</Phrase>
-      </motion.div>
-    </>
-  );
+				<Phrase>Each of our lives is a different way of seeing</Phrase>
+			</motion.div>
+		</>
+	);
 };
 export default Home;
--- src/components/WhatNow.js
+++ src/components/WhatNow.js
@@ -1,23 +1,53 @@
 import React from 'react';
 import HorizontalScroll from 'react-scroll-horizontal';
+import img1 from '../images/WEB-6417--2000x1333--256kb.jpg';
+
+// div parent
+// transform: translate3d(0px, 0px, 0px);
+//     display: flex;
+//     height: 100%;
+//     position: absolute;
+//     will-change: transform;
+//     justify-content: start;
+
+// img
+// width: 17%; //can vary depending on amount of images in array
+//     /* background-color: red; */
+//     /* margin: 40px; */
+//     height: auto;
+//     object-fit: contain;
+//
 
 const WhatNow = () => {
-	const child = { width: `30em`, height: `100%`, backgroundColor: 'red' };
+	const child = {
+		width: `100vw`,
+		height: `auto`,
+		backgroundColor: 'red',
+
+		margin: 40,
+	};
 
 	return (
-		<div style={{ backgroundColor: 'aqua', padding: 40 }}>
-			<HorizontalScroll>
-				<div style={child}>
-					<h1>hello</h1>
-				</div>
-				<div style={child}>
-					<h1>hello</h1>
-				</div>
-				<div style={child}>
-					<h1>hello</h1>
-				</div>
-			</HorizontalScroll>
-		</div>
+		<>
+			<h1 style={{ position: 'absolute', zIndex: 1 }}>Hello</h1>
+			<div style={{ backgroundColor: 'aqua', height: '100vh' }}>
+				<HorizontalScroll>
+					<div style={child}>
+						<div>
+							<img style={{ height: '90%' }} src={img1} alt='' />
+							<p>hello ser how are you</p>
+							<img style={{ height: '70%' }} src={img1} alt='' />
+						</div>
+					</div>
+					<div style={child}>
+						<img style={{ height: '90%' }} src={img1} alt='' />
+					</div>
+					<div style={child}>
+						<img style={{ height: '90%' }} src={img1} alt='' />
+					</div>
+				</HorizontalScroll>
+			</div>
+		</>
 	);
 };
 
