186f8f99d0bb8081117eabb06eeba957eaf7faa0
Amel Ramirez <amelramirez@192.168.2.55>
2021-02-15 22:39:59 +0100

    flowers almost done

--- src/pages/Latf/dataLatf.js
+++ src/pages/Latf/dataLatf.js
@@ -1,248 +1,318 @@
-import W0241 from "../../images/LATF/WEB-0241-281kb.jpg";
-import W0291 from "../../images/LATF/WEB-0291-251kb.jpg";
-import W0563 from "../../images/LATF/WEB-0563-247kb.jpg";
-import W0900 from "../../images/LATF/WEB-0900-247kb.jpg";
-import W2499 from "../../images/LATF/WEB-2499-249kb.jpg";
+import W0241 from '../../images/LATF/WEB-0241-281kb.jpg';
+import W0291 from '../../images/LATF/WEB-0291-251kb.jpg';
+import W0563 from '../../images/LATF/WEB-0563-247kb.jpg';
+import W0900 from '../../images/LATF/WEB-0900-247kb.jpg';
+import W2499 from '../../images/LATF/WEB-2499-249kb.jpg';
 
 ///////
-import W3881 from "../../images/LATF/WEB-3881-251kb.jpg";
-import W4223 from "../../images/LATF/WEB-4223-249kb.jpg";
-import W4243 from "../../images/LATF/WEB-4243-243kb.jpg";
+import W3881 from '../../images/LATF/WEB-3881-251kb.jpg';
+import W4223 from '../../images/LATF/WEB-4223-249kb.jpg';
+import W4243 from '../../images/LATF/WEB-4243-243kb.jpg';
 ///////
-import W3887 from "../../images/LATF/WEB-3887-288kb.jpg";
+import W3887 from '../../images/LATF/WEB-3887-288kb.jpg';
 ///////
-import W6417 from "../../images/LATF/WEB-6417-256kb.jpg";
-import W2348 from "../../images/LATF/WEB 2348-260kb.jpg";
+import W6417 from '../../images/LATF/WEB-6417-256kb.jpg';
+import W2348 from '../../images/LATF/WEB 2348-260kb.jpg';
 ///////
-import W2613 from "../../images/LATF/WEB-2613-257kb.jpg";
+import W2613 from '../../images/LATF/WEB-2613-257kb.jpg';
 ///////
-import W8135 from "../../images/LATF/WEB-8135-249kb.jpg";
+import W8135 from '../../images/LATF/WEB-8135-249kb.jpg';
 ///////
-import W7020 from "../../images/LATF/WEB-7020-254kb.jpg";
+import W7020 from '../../images/LATF/WEB-7020-254kb.jpg';
 ///////
-import W3589 from "../../images/LATF/WEB-3589-249kb.jpg";
+import W3589 from '../../images/LATF/WEB-3589-249kb.jpg';
 ///////
-import W9598 from "../../images/LATF/WEB-9598-250kb.jpg";
-import W9486 from "../../images/LATF/WEB-9486-313kb.jpg";
-import W9590 from "../../images/LATF/WEB-9590-250kb.jpg";
+import W9598 from '../../images/LATF/WEB-9598-250kb.jpg';
+import W9486 from '../../images/LATF/WEB-9486-313kb.jpg';
+import W9590 from '../../images/LATF/WEB-9590-250kb.jpg';
 ///////
-import N7A7897 from "../../images/LATF/WEB-2N7A7897-255kb.jpg";
+import N7A7897 from '../../images/LATF/WEB-2N7A7897-255kb.jpg';
 ///////
-import W5841 from "../../images/LATF/WEB-5841-275kb.jpg";
+import W5841 from '../../images/LATF/WEB-5841-275kb.jpg';
 ///////
-import W5075 from "../../images/LATF/WEB-5075-264kb.jpg";
-import W4590 from "../../images/LATF/WEB-4590-255kb.jpg";
-import W1763 from "../../images/LATF/WEB-1763-296kb.jpg";
-import W4799 from "../../images/LATF/WEB-4799-252kb.jpg";
+import W5075 from '../../images/LATF/WEB-5075-264kb.jpg';
+import W4590 from '../../images/LATF/WEB-4590-255kb.jpg';
+import W1763 from '../../images/LATF/WEB-1763-296kb.jpg';
+import W4799 from '../../images/LATF/WEB-4799-252kb.jpg';
 ///////
-import W4767 from "../../images/LATF/WEB-4767-264kb.jpg";
+import W4767 from '../../images/LATF/WEB-4767-264kb.jpg';
 ///////
-import W0347 from "../../images/LATF/WEB-0347-241kb.jpg";
-import W5679 from "../../images/LATF/WEB-5679-295kb.jpg";
-import W2421 from "../../images/LATF/WEB-2421-254kb.jpg";
+import W0347 from '../../images/LATF/WEB-0347-241kb.jpg';
+import W5679 from '../../images/LATF/WEB-5679-295kb.jpg';
+import W2421 from '../../images/LATF/WEB-2421-254kb.jpg';
 ///////
-import W0362 from "../../images/LATF/WEB-0362-260kb.jpg";
+import W0362 from '../../images/LATF/WEB-0362-260kb.jpg';
 ///////
-import W3838 from "../../images/LATF/WEB-3838-249kb.jpg";
+import W3838 from '../../images/LATF/WEB-3838-249kb.jpg';
 ///////
-import W6408 from "../../images/LATF/WEB-6408-257kb.jpg";
+import W6408 from '../../images/LATF/WEB-6408-257kb.jpg';
 ///////
-import W0430 from "../../images/LATF/WEB-0430-252kb.jpg";
-import W0907 from "../../images/LATF/Web-0907-251kb.jpg";
-import W6897 from "../../images/LATF/WEB-6897-248kb.jpg";
+import W0430 from '../../images/LATF/WEB-0430-252kb.jpg';
+import W0907 from '../../images/LATF/Web-0907-251kb.jpg';
+import W6897 from '../../images/LATF/WEB-6897-248kb.jpg';
 ///////
-import W2N7A8251 from "../../images/LATF/WEB-2N7A8251-250kb.jpg";
+import W2N7A8251 from '../../images/LATF/WEB-2N7A8251-250kb.jpg';
 ///////
-import W9225 from "../../images/LATF/WEB-9225-250kb.jpg";
-import W9387 from "../../images/LATF/WEB-9387-257kb.jpg";
+import W9225 from '../../images/LATF/WEB-9225-250kb.jpg';
+import W9387 from '../../images/LATF/WEB-9387-257kb.jpg';
 ///////
-import W4447 from "../../images/LATF/WEB-4447-253kb.jpg";
+import W4447 from '../../images/LATF/WEB-4447-253kb.jpg';
 ///////
-import W9170 from "../../images/LATF/WEB-9170-259kb.jpg";
-import W9171 from "../../images/LATF/WEB-9171-251kb.jpg";
-import W9059 from "../../images/LATF/WEB-9059-258kb.jpg";
+import W9170 from '../../images/LATF/WEB-9170-259kb.jpg';
+import W9171 from '../../images/LATF/WEB-9171-251kb.jpg';
+import W9059 from '../../images/LATF/WEB-9059-258kb.jpg';
 ///////
-import W2N7A8243 from "../../images/LATF/WEB-2N7A8243-249kb.jpg";
+import W2N7A8243 from '../../images/LATF/WEB-2N7A8243-249kb.jpg';
 ///////
-import W1571 from "../../images/LATF/WEB-1571-250kb.jpg";
-import W0713 from "../../images/LATF/WEB-0713-251kb.jpg";
-import W2N7A8110 from "../../images/LATF/WEB-2N7A8110-282kb.jpg";
+import W1571 from '../../images/LATF/WEB-1571-250kb.jpg';
+import W0713 from '../../images/LATF/WEB-0713-251kb.jpg';
+import W2N7A8110 from '../../images/LATF/WEB-2N7A8110-282kb.jpg';
 ///////
-import W1059 from "../../images/LATF/WEB-1059-249kb.jpg";
+import W1059 from '../../images/LATF/WEB-1059-249kb.jpg';
 ///////
-import W1669 from "../../images/LATF/WEB-1669-254kb.jpg";
-import W1111 from "../../images/LATF/WEB-1111-251kb.jpg";
+import W1669 from '../../images/LATF/WEB-1669-254kb.jpg';
+import W1111 from '../../images/LATF/WEB-1111-251kb.jpg';
 ///////
-import W0831 from "../../images/LATF/WEB-0831-252kb.jpg";
+import W0831 from '../../images/LATF/WEB-0831-252kb.jpg';
 
 export const Tulips19 = {
-  name: "Tulips...2019",
-  text: "",
-  page: [
-    { id: 1, image: W0241, css: { width: "20%", margin: 10 } },
-    { id: 2, image: W0291, css: { width: "40%", margin: 10 } },
-    { id: 3, image: W0563, css: { width: "20%", margin: 10 } },
-    { id: 4, image: W0900, css: { width: "20%", margin: 10 } },
-    { id: 5, image: W2499, css: { width: "20%", margin: 10 } },
-  ],
+	name: 'Tulips...2019',
+	text: '',
+	page: [
+		{ id: 1, image: W0241, css: { width: '20%', margin: 10 } },
+		{ id: 2, image: W0291, css: { width: '38%', margin: 10 } },
+		{ id: 3, image: W0563, css: { width: '20%', margin: 10 } },
+		{ id: 4, image: W0900, css: { width: '20%', margin: 10 } },
+		{ id: 5, image: W2499, css: { width: '20%', margin: 10 } },
+	],
 };
 export const Tulips20 = {
-  name: "Tulips...2020",
-  text: "",
-  page: [
-    { id: 1, image: W4223, css: { width: "20%", margin: 10 } },
-    { id: 2, image: W4243, css: { width: "40%", margin: 10 } },
-    { id: 3, image: W3881, css: { width: "20%", margin: 10 } },
-  ],
+	name: 'Tulips...2020',
+	text: '',
+	page: [
+		{
+			id: 1,
+			image: W4223,
+			css: {
+				width: '20%',
+				margin: 10,
+				display: 'flex',
+				alignSelf: 'flex-start',
+			},
+		},
+		{ id: 2, image: W4243, css: { width: '24%', margin: 10 } },
+		{
+			id: 3,
+			image: W3881,
+			css: {
+				width: '20%',
+				margin: 10,
+				display: 'flex',
+				alignSelf: 'flex-start',
+			},
+		},
+	],
 };
 export const Tulips20A = {
-  name: "Tulips...2020",
-  text: "",
-  page: [{ id: 1, image: W3887, css: { width: "20%", margin: 10 } }],
+	name: 'Tulips...2020',
+	text: '',
+	page: [{ id: 1, image: W3887, css: { width: '28%', margin: 10 } }],
 };
 export const BAON = {
-  name: "By Any Other Name",
-  text: "",
-  page: [
-    { id: 1, image: W6417, css: { width: "20%", margin: 10 } },
-    { id: 2, image: W2348, css: { width: "20%", margin: 10 } },
-  ],
+	name: 'By Any Other Name',
+	text: '',
+	page: [
+		{
+			id: 1,
+			image: W6417,
+			css: {
+				width: '40%',
+				margin: 10,
+				display: 'flex',
+				alignSelf: 'flex-start',
+			},
+		},
+		{
+			id: 2,
+			image: W2348,
+			css: { width: '40%', margin: 10, display: 'flex', alignSelf: 'flex-end' },
+		},
+	],
 };
 export const THOAA = {
-  name: "The Heart of an Artist",
-  text: "",
-  page: [{ id: 1, image: W2613, css: { width: "20%", margin: 10 } }],
+	name: 'The Heart of an Artist',
+	text: '',
+	page: [{ id: 1, image: W2613, css: { width: '60%', margin: 10 } }],
 };
 export const RW = {
-  name: "Rose Window...",
-  text:
-    "My love for you is an evening star that glows once more toward the end of day My love for you is an ancient scar a gash in youth that bled away My love for you is the morning star the glint of light that leads the way",
-  page: [{ id: 1, image: W8135, css: { width: "20%", margin: 10 } }],
+	name: 'Rose Window...',
+	page: [{ id: 1, image: W8135, css: { width: '50%', margin: 10 } }],
 };
 export const Intimations = {
-  name: "Intimations",
-  text: "",
-  page: [{ id: 1, image: W7020, css: { width: "20%", margin: 10 } }],
+	name: 'Intimations',
+	text: '',
+	page: [{ id: 1, image: W7020, css: { width: '60%', margin: 10 } }],
 };
-// export const HC = {
-//   name: "Heirloom Corsage",
-//   text: "",
-//   p8: [{ id: 1, image: W3589, css: { width: "20%", margin: 10 } }],
-// };
-// export const WOF = {
-//   name: "When Orchids Fly",
-//   text: "",
-//   p9: [
-//     { id: 1, image: W9598, css: { width: "20%", margin: 10 } },
-//     { id: 2, image: W9486, css: { width: "20%", margin: 10 } },
-//     { id: 3, image: W9590, css: { width: "20%", margin: 10 } },
-//   ],
-// };
-// export const Annunciation = {
-//   name: "Annunciation",
-//   text: "",
-//   p10: [{ id: 1, image: N7A7897, css: { width: "30%", margin: 10 } }],
-// };
-// export const Windhover = {
-//   name: "Windhover",
-//   text: "",
-//   p11: [{ id: 1, image: W5841, css: { width: "80%", margin: 10 } }],
-// };
-// export const TA = {
-//   name: "The Alium",
-//   text: "",
-//   p12: [
-//     { id: 1, image: W5075, css: { width: "20%", margin: 10 } },
-//     { id: 2, image: W4590, css: { width: "20%", margin: 10 } },
-//     { id: 3, image: W1763, css: { width: "20%", margin: 10 } },
-//     { id: 4, image: W4799, css: { width: "20%", margin: 10 } },
-//   ],
-// };
-// export const TAA = {
-//   name: "The Alium",
-//   text: "",
-//   p13: [{ id: 1, image: W4767, css: { width: "80%", margin: 10 } }],
-// };
-// export const Peonies = {
-//   name: "Peonies",
-//   text: "",
-//   p14: [
-//     { id: 1, image: W0347, css: { width: "20%", margin: 10 } },
-//     { id: 2, image: W5679, css: { width: "20%", margin: 10 } },
-//     { id: 3, image: W2421, css: { width: "20%", margin: 10 } },
-//   ],
-// };
-// export const PeoniesA = {
-//   name: "Peonies",
-//   text: "",
-//   p15: [{ id: 1, image: W0362, css: { width: "20%", margin: 10 } }],
-// };
-// export const HTM = {
-//   name: "Homage to Monet",
-//   text: "",
-//   p16: [{ id: 1, image: W3838, css: { width: "30%", margin: 10 } }],
-// };
-// export const ITSOTN = {
-//   name: "In the Still of the Night",
-//   text:
-//     "Like the moon growing dim on the rim of the hill in the chill,still of the night... Cole Porter",
-//   p17: [{ id: 1, image: W6408, css: { width: "30%", margin: 10 } }],
-// };
-// export const UG = {
-//   name: "Unexpected Guests",
-//   text: "",
-//   p18: [
-//     { id: 1, image: W0430, css: { width: "20%", margin: 10 } },
-//     { id: 2, image: W0907, css: { width: "20%", margin: 10 } },
-//     { id: 3, image: W6897, css: { width: "20%", margin: 10 } },
-//   ],
-// };
-// export const Alien = {
-//   name: "Alien",
-//   text: "",
-//   p19: [{ id: 1, image: W2N7A8251, css: { width: "30%", margin: 10 } }],
-// };
-// export const Animations = {
-//   name: "Animations",
-//   text: "",
-//   p20: [
-//     { id: 1, image: W9225, css: { width: "20%", margin: 10 } },
-//     { id: 2, image: W9387, css: { width: "20%", margin: 10 } },
-//   ],
-// };
-// export const HTGO = {
-//   name: "Homage to Georgia O'Keefe",
-//   text: "",
-//   p21: [{ id: 1, image: W4447, css: { width: "20%", margin: 10 } }],
-// };
-// export const Chrysanthemums = {
-//   name: "Animations",
-//   text: "",
-//   p22: [
-//     { id: 1, image: W9170, css: { width: "20%", margin: 10 } },
-//     { id: 2, image: W9171, css: { width: "20%", margin: 10 } },
-//     { id: 3, image: W9059, css: { width: "20%", margin: 10 } },
-//   ],
-// };
-// export const UL = {
-//   name: "Unstill Life",
-//   text: "",
-//   p23: [{ id: 1, image: W2N7A8243, css: { width: "20%", margin: 10 } }],
-// };
-// export const Hydrangeas = {
-//   name: "Hydrangeas",
-//   text: "",
-//   p24: [
-//     { id: 1, image: W1571, css: { width: "20%", margin: 10 } },
-//     { id: 2, image: W0713, css: { width: "20%", margin: 10 } },
-//     { id: 3, image: W2N7A8110, css: { width: "20%", margin: 10 } },
-//   ],
-// };
-// export const HTMA = {
-//   name: "Unstill Life",
-//   text: "",
-//   p25: [{ id: 1, image: W1059, css: { width: "20%", margin: 10 } }],
-// };
+export const HC = {
+	name: 'Heirloom Corsage',
+	text: '',
+	page: [{ id: 1, image: W3589, css: { width: '60%', margin: 10 } }],
+};
+export const WOF = {
+	name: 'When Orchids Fly',
+	text: '',
+	page: [
+		{
+			id: 1,
+			image: W9598,
+			css: { width: '20%', margin: 10, display: 'flex', alignSelf: 'flex-end' },
+		},
+		{ id: 2, image: W9486, css: { width: '24%', margin: 10 } },
+		{
+			id: 3,
+			image: W9590,
+			css: {
+				width: '20%',
+				margin: 10,
+				display: 'flex',
+				alignSelf: 'flex-start',
+			},
+		},
+	],
+};
+export const Annunciation = {
+	name: 'Annunciation',
+	text: '',
+	page: [{ id: 1, image: N7A7897, css: { width: '28%', margin: 10 } }],
+};
+export const Windhover = {
+	name: 'Windhover',
+	text:
+		'To Christ our Lord I caught this morning morning’s minion, kingdom of daylight’s dauphin, dapple-dawn-drawn Falcon, in his riding Of the rolling level underneath him steady air, and striding High there, how he rung upon the rein of a wimpling wing In his ecstacy! then off, off forth on swing, As a skate’s heel sweeps smooth on a bow-bend: the hurl and gliding Rebuffed the big wind. My heart in hiding Stirred for a bird,—the achieve of, the mastery of the thing! Brute beauty and valour and act, oh, air, pride, plume, here Buckle! AND the fire that breaks from thee then, a billion Times told lovelier, more dangerous, O my chevalier! No wonder of it: shéer plód makes plough down sillion Shine, and blue-bleak embers, ah my dear, Fall, gall themselves, and gash gold-vermillion.',
+	p11: [{ id: 1, image: W5841, css: { width: '80%', margin: 10 } }],
+};
+
+export const TA = {
+	name: 'The Alium',
+	text: '',
+	p12: [
+		{ id: 1, image: W5075, css: { width: '18%', margin: 10 } },
+		{ id: 2, image: W4590, css: { width: '38%' } },
+		{ id: 3, image: W1763, css: { width: '38%' } },
+		{ id: 4, image: W4799, css: { width: '18%', margin: 10 } },
+	],
+};
+export const TAA = {
+	name: 'The Alium',
+	text: '',
+	p13: [{ id: 1, image: W4767, css: { width: '60%', margin: 10 } }],
+};
+export const Peonies = {
+	name: 'Peonies',
+	text: '',
+	p14: [
+		{ id: 1, image: W0347, css: { width: '28%' } },
+		{ id: 2, image: W5679, css: { width: '50%', margin: 10 } },
+		{ id: 3, image: W2421, css: { width: '50%', margin: 10 } },
+	],
+};
+export const PeoniesA = {
+	name: 'Peonies',
+	text: '',
+	page: [{ id: 1, image: W0362, css: { width: '60%', margin: 10 } }],
+};
+export const HTM = {
+	name: 'Homage to Monet',
+	text: '',
+	page: [{ id: 1, image: W3838, css: { width: '28%', margin: 10 } }],
+};
+export const ITSOTN = {
+	name: 'In the Still of the Night',
+	text:
+		'Like the moon growing dim on the rim of the hill in the chill, still of the night... Cole Porter',
+	p17: [{ id: 1, image: W6408, css: { width: '30%', margin: 10 } }],
+};
+export const UG = {
+	name: 'Unexpected Guests',
+	text: '',
+	page: [
+		{
+			id: 1,
+			image: W0430,
+			css: {
+				width: '25%',
+				margin: 10,
+				display: 'flex',
+				alignSelf: 'flex-start',
+			},
+		},
+		{ id: 2, image: W0907, css: { width: '25%' } },
+		{
+			id: 3,
+			image: W6897,
+			css: { width: '25%', margin: 10, display: 'flex', alignSelf: 'flex-end' },
+		},
+	],
+};
+export const Alien = {
+	name: 'Alien',
+	text: '',
+	page: [{ id: 1, image: W2N7A8251, css: { width: '60%', margin: 10 } }],
+};
+export const Animations = {
+	name: 'Animations',
+	text: '',
+	page: [
+		{
+			id: 1,
+			image: W9225,
+			css: { width: '34%', margin: 10, display: 'flex', alignSelf: 'flex-end' },
+		},
+		{
+			id: 2,
+			image: W9387,
+			css: {
+				width: '34%',
+				margin: 10,
+				display: 'flex',
+				alignSelf: 'flex-start',
+			},
+		},
+	],
+};
+export const HTGO = {
+	name: "Homage to Georgia O'Keefe",
+	text: '',
+	page: [{ id: 1, image: W4447, css: { width: '28%', margin: 10 } }],
+};
+export const Chrysanthemums = {
+	name: 'Chrysanthemums',
+	text: '',
+	page: [
+		{ id: 1, image: W9170, css: { width: '20%', margin: 10 } },
+		{ id: 2, image: W9171, css: { width: '20%', margin: 10 } },
+		{ id: 3, image: W9059, css: { width: '20%', margin: 10 } },
+	],
+};
+export const UL = {
+	name: 'Unstill Life',
+	text: '',
+	page: [{ id: 1, image: W2N7A8243, css: { width: '60%', margin: 10 } }],
+};
+export const Hydrangeas = {
+	name: 'Hydrangeas',
+	text: '',
+	page: [
+		{ id: 1, image: W1571, css: { width: '20%', margin: 10 } },
+		{ id: 2, image: W0713, css: { width: '20%', margin: 10 } },
+		{ id: 3, image: W2N7A8110, css: { width: '20%', margin: 10 } },
+	],
+};
+export const HTMA = {
+	name: 'Homage to Mandelbrot',
+	text: '',
+	page: [{ id: 1, image: W1059, css: { width: '60%', margin: 10 } }],
+};
 // export const LOTF = {
 //   name: "Lilies of the Field",
 //   text:
@@ -258,32 +328,37 @@ export const Intimations = {
 //   p27: [{ id: 1, image: W0831, css: { width: "20%", margin: 10 } }],
 // };
 
-export const objects = [
-  Tulips19,
-  Tulips20,
-  Tulips20A,
-  BAON,
-  THOAA,
-  RW,
-  Intimations,
-  //   HC,
-  //   WOF,
-  //   Annunciation,
-  //   Windhover,
-  //   TA,
-  //   TAA,
-  //   Peonies,
-  //   PeoniesA,
-  //   HTM,
-  //   ITSOTN,
-  //   UG,
-  //   Alien,
-  //   Animations,
-  //   HTGO,
-  //   Chrysanthemums,
-  //   UL,
-  //   Hydrangeas,
-  //   HTMA,
-  //   LOTF,
-  //   HTRM,
+export const objects1 = [
+	Tulips19,
+	Tulips20,
+	Tulips20A,
+	BAON,
+	THOAA,
+
+	//   Windhover,
+
+	//   ITSOTN,
+	//   UG,
+	//   Alien,
+	//   Animations,
+	//   HTGO,
+	//   Chrysanthemums,
+	//   UL,
+	//   Hydrangeas,
+	//   HTMA,
+	//   LOTF,
+	//   HTRM,
+];
+
+export const objects2 = [Intimations, HC, WOF, Annunciation];
+export const objects3 = [PeoniesA, HTM];
+export const objects4 = [
+	UG,
+	Alien,
+	Animations,
+	HTGO,
+	Chrysanthemums,
+	UL,
+	Hydrangeas,
+	HTMA,
 ];
--- src/pages/Latf/styles.jsx
+++ src/pages/Latf/styles.jsx
@@ -1,25 +1,63 @@
 import styled from 'styled-components';
+import { device } from '../../generalStyles';
 
 export const Container = styled.div`
 	width: 100vw;
 	height: 100%;
-	// background-color: red;
 	display: flex;
 	flex-direction: column;
 	justify-content: flex-end;
 	align-items: center;
 `;
+
+export const FotoWrapper = styled.div`
+	display: flex;
+	justify-content: center;
+`;
+export const PeoniesWrapper = styled.div`
+	display: flex;
+	justify-content: center;
+`;
+
 export const Wrapper = styled.div`
 	display: flex;
 	// flex-direction: column;
 	justify-content: center;
 	align-items: center;
 	flex-wrap: wrap;
-	height: 85%;
+	height: 80%;
+	${FotoWrapper}:nth-child(2) {
+		width: 50%;
+	}
+	${FotoWrapper}:nth-child(3) {
+		width: 50%;
+	}
+	${PeoniesWrapper}:nth-child(2) {
+		width: 50%;
+	}
+	${PeoniesWrapper}:nth-child(3) {
+		width: 50%;
+	}
 `;
 
 export const SubHeader = styled.h2`
 	margin: 0;
-	font: 300 26px / 30px Lato;
+	font: 300 16px/16px Lato;
 	color: #331c65;
+`;
+
+export const ImageWrapper = styled.figure`
+	width: fit-content;
+	height: fit-content;
+	align-items: center;
+	background-color: antiquewhite;
+	padding: 20px;
+	max-width: 30%;
+	display: flex;
+	flex-direction: column;
+	align-self: center;
+	@media ${device.medium} {
+		padding: 30px;
+		max-width: 30%;
+	}
 `;
