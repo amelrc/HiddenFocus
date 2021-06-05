ac66e72d7b2fe666ce50e1a2dc4ce036630e42e0
Amel Ramirez <amelramirez@Amels-iMac.local>
2021-06-05 09:02:03 +0200

    log

--- src/pages/Latf/Latf.jsx
+++ src/pages/Latf/Latf.jsx
@@ -39,6 +39,8 @@ import {
 
 const transition = { duration: 1.4, ease: [0.6, 0.01, -0.05, 0.9] };
 
+console.log("hello");
+
 const Latf = () => {
   const [showModal, setShowModal] = useState(false);
   const [selected, setSelected] = useState(0);
